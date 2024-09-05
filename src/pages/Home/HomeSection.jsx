9ab9f1d176f4433ff9ce5f1b73f028c777168957
SabinaGarayeva <sgarayeva13@gmail.com>
2024-09-05 19:02:03 +0400

    minor changes in homepage + footer part ( static)

--- src/pages/Home/HomeSection.jsx
+++ src/pages/Home/HomeSection.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import LightBulb from "../../assets/images/light.png";
 import Arrow from "../../assets/images/arrow.png";
@@ -8,6 +8,9 @@ import Weakness from "../../assets/images/weakness.png";
 import Math from "../../assets/images/math.png";
 import Math2 from "../../assets/images/math2.png";
 import Azlang from "../../assets/images/azlang.png";
+import HomeAvatar from "../../assets/images/HomeAvatar.png";
+import Badge from "../../assets/images/badge.png";
+import Footer from "../../components/layout/Footer";
 
 export const HomeSection = () => {
   return (
@@ -17,7 +20,7 @@ export const HomeSection = () => {
           direction="column"
           justifyContent="center"
           alignItems="center"
-          mb="50px"
+          mb="100px"
         >
           <Heading as="h3" size="lg" mb={8}>
             BİZİMLƏ SINA!
@@ -78,7 +81,7 @@ export const HomeSection = () => {
           direction="column"
           justifyContent="center"
           alignItems="center"
-          mb="50px"
+          mb="100px"
         >
           <Heading as="h3" size="lg" mb="100px">
             BİZİM SERVİSLƏR!
@@ -163,7 +166,12 @@ export const HomeSection = () => {
       </section>
 
       <section>
-        <Flex direction="column" justifyContent="center" alignItems="center">
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          mb="100px"
+        >
           <Heading as="h3" size="lg" mb={8}>
             TOP FƏNLƏR!
           </Heading>
@@ -234,12 +242,265 @@ export const HomeSection = () => {
       </section>
 
       <section>
-        <Flex direction="column" justifyContent="center" alignItems="center">
-          <Heading as="h3" size="lg" mb={8}>
-             LİDERLƏR LÖVHƏSİ!
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          mb="100px"
+        >
+          <Heading as="h3" size="lg" mb="100px">
+            LİDERLƏR LÖVHƏSİ!
           </Heading>
+
+          <Flex
+            justifyContent="center"
+            gap={{ base: "60px", lg: 4 }}
+            direction={{ base: "column", lg: "row" }}
+            alignItems="center"
+          >
+            <Box
+              as="section"
+              w="380px"
+              h="250px"
+              borderRadius="10px"
+              border="3px solid #4AE49D"
+              position="relative"
+            >
+              <Image
+                src={Badge}
+                position="absolute"
+                bg="white"
+                borderRadius="100%"
+                border="8px solid white"
+                top="-25px"
+                left="20px"
+              />
+
+              <Image
+                src={HomeAvatar}
+                position="absolute"
+                top="25px"
+                left="155px"
+              />
+              <Box h="70px" bg="#4AE49E" mb={10} />
+
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                gap={2}
+                direction="column"
+                textAlign="center" // Added textAlign="center" to center text
+              >
+                <Heading as="h5" size="md">
+                  A. Orucov
+                </Heading>
+                <Text>
+                  Riyaziyyat fənnindən sınaqlarda May ayı ərzində göstərdiyi
+                  nəticə
+                </Text>
+                <Heading as="h1" color="#4AE49E">
+                  20/20
+                </Heading>
+              </Flex>
+            </Box>
+
+            <Box
+              as="section"
+              w="380px"
+              h="250px"
+              borderRadius="10px"
+              border="3px solid #FFA753"
+              position="relative"
+            >
+              <Image
+                src={Badge}
+                position="absolute"
+                bg="white"
+                borderRadius="100%"
+                border="8px solid white"
+                top="-25px"
+                left="20px"
+              />
+
+              <Image
+                src={HomeAvatar}
+                position="absolute"
+                top="25px"
+                left="155px"
+              />
+              <Box h="70px" bg="#FFA753" mb={10} />
+
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                gap={2}
+                direction="column"
+                textAlign="center" // Added textAlign="center" to center text
+              >
+                <Heading as="h5" size="md">
+                  A. Orucov
+                </Heading>
+                <Text>
+                  Riyaziyyat fənnindən sınaqlarda May ayı ərzində göstərdiyi
+                  nəticə
+                </Text>
+                <Heading as="h1" color="#FFA753">
+                  20/20
+                </Heading>
+              </Flex>
+            </Box>
+
+            <Box
+              as="section"
+              w="380px"
+              h="250px"
+              borderRadius="10px"
+              border="3px solid #28B5FF"
+              position="relative"
+            >
+              <Image
+                src={Badge}
+                position="absolute"
+                bg="white"
+                borderRadius="100%"
+                border="8px solid white"
+                top="-25px"
+                left="20px"
+              />
+
+              <Image
+                src={HomeAvatar}
+                position="absolute"
+                top="25px"
+                left="155px"
+              />
+              <Box h="70px" bg="#28B5FF" mb={10} />
+
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                gap={2}
+                direction="column"
+                textAlign="center" // Added textAlign="center" to center text
+              >
+                <Heading as="h5" size="md">
+                  A. Orucov
+                </Heading>
+                <Text>
+                  Riyaziyyat fənnindən sınaqlarda May ayı ərzində göstərdiyi
+                  nəticə
+                </Text>
+                <Heading as="h1" color="#28B5FF">
+                  20/20
+                </Heading>
+              </Flex>
+            </Box>
+          </Flex>
         </Flex>
       </section>
+
+      <section>
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          mb="100px"
+        >
+          <Heading as="h3" size="lg" mb="100px">
+            TƏHSİL PAKETLƏRİMİZ
+          </Heading>
+
+          <Flex
+            justifyContent="center"
+            gap={4}
+            direction={{ base: "column", lg: "row" }}
+            alignItems="center"
+          >
+            <Box
+              bg="transparent"
+              as="section"
+              w="380px"
+              p="30px"
+              borderRadius="10px"
+              border="1px solid black"
+            >
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                // gap={8}
+                direction="column"
+                textAlign="center" // Added textAlign="center" to center text
+              >
+                <Heading as="h5" size="md">
+                  Standart Paket
+                </Heading>
+                <Heading as="h5" size="lg" mt="40px">
+                  $80 / Ay
+                </Heading>
+                <Text mt="25px">Lorem ipsum dolor sit amet consectetur.</Text>
+                <Button bg="#A4D7F2" w="100%" mt="40px">
+                  QEYDIYYATDAN KEÇ
+                </Button>
+              </Flex>
+            </Box>
+            <Box
+              bg="transparent"
+              as="section"
+              w="380px"
+              p="30px"
+              borderRadius="10px"
+              border="1px solid black"
+            >
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                // gap={8}
+                direction="column"
+                textAlign="center" // Added textAlign="center" to center text
+              >
+                <Heading as="h5" size="md">
+                  Standart Paket
+                </Heading>
+                <Heading as="h5" size="lg" mt="40px">
+                  $80 / Ay
+                </Heading>
+                <Text mt="25px">Lorem ipsum dolor sit amet consectetur.</Text>
+                <Button bg="#A4D7F2" w="100%" mt="40px">
+                  QEYDIYYATDAN KEÇ
+                </Button>
+              </Flex>
+            </Box>
+            <Box
+              bg="transparent"
+              as="section"
+              w="380px"
+              p="30px"
+              borderRadius="10px"
+              border="1px solid black"
+            >
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                // gap={8}
+                direction="column"
+                textAlign="center" // Added textAlign="center" to center text
+              >
+                <Heading as="h5" size="md">
+                  Standart Paket
+                </Heading>
+                <Heading as="h5" size="lg" mt="40px">
+                  $80 / Ay
+                </Heading>
+                <Text mt="25px">Lorem ipsum dolor sit amet consectetur.</Text>
+                <Button bg="#A4D7F2" w="100%" mt="40px">
+                  QEYDIYYATDAN KEÇ
+                </Button>
+              </Flex>
+            </Box>
+          </Flex>
+        </Flex>
+      </section>
+      <Footer />
     </>
   );
 };
