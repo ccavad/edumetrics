e2d211f63dafc6c5e199af5b0e0da519fa007233
SabinaGarayeva <sgarayeva13@gmail.com>
2024-09-06 15:47:38 +0400

    delete unnecessary items

--- src/pages/Home/HomeSection.jsx
+++ src/pages/Home/HomeSection.jsx
@@ -1,22 +1,8 @@
-import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import LightBulb from "../../assets/images/light.png";
-import Arrow from "../../assets/images/arrow.png";
-import Books from "../../assets/images/books.png";
-import Presentation from "../../assets/images/presentation.png";
-import Weakness from "../../assets/images/weakness.png";
-import Math from "../../assets/images/math.png";
-import Math2 from "../../assets/images/math2.png";
-import Azlang from "../../assets/images/azlang.png";
-import HomeAvatar from "../../assets/images/HomeAvatar.png";
-import Badge from "../../assets/images/badge.png";
 import FeatureCardSection from "./FeatureCardSection";
-import ServiceCardSection from "./ServiceCardSection";
 import LeadersSection from "./LeaderSection";
 import PricingSection from "./PricingSection";
-
-
-
+import ServiceCardSection from "./ServiceCardSection";
 
 
 export const HomeSection = () => {
