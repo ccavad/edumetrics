02d1981cb9cc34bcd12482072592021726d32229
ccavad <88976928+ccavad@users.noreply.github.com>
2024-09-25 23:33:26 +0400

    path typo fixed for deployment

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
-import { AuthTitleSection } from "../../components/form/authTitleSection";
+import { AuthTitleSection } from "../../components/form/AuthTitleSection";
 import { textTemplates } from "../../utils/statics/templates";
 import { LoginForm } from "./LoginForm";
 
